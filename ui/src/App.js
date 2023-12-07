22ff0bbe6336c134f8a64b6a951c7e7191186e2f
Jonah Ludke <jonahludke@gmail.com>
2023-12-07 14:00:19 -0500

    Feat: Button adds value to cell

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,56 +1,57 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import * as d3 from 'd3';
+
 
 const Heatmap = () => {
   const d3Chart = useRef();
+  const [data, setData] = useState([
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+  ]);
 
   useEffect(() => {
     drawHeatmap();
-  }, []);
+  }, [data]);
 
   const drawHeatmap = () => {
-    // Sample data
-    const data = [
-      [10, 20, 30, 20, 23, 14, 43],
-      [15, 25, 35, 30],
-      [20, 30, 40, 50],
-      [20, 30, 40],
-      [20, 30, 40],
-      [20, 30, 40, 54, 45, 21, 56],
-    ];
+    // Clear the SVG on each redraw
+    d3.select(d3Chart.current).selectAll('*').remove();
 
     const gridSize = 100;
     const margin = { top: 20, right: 30, bottom: 30, left: 40 };
     const width = data[0].length * gridSize;
     const height = data.length * gridSize;
 
-    const colorScale = d3.scaleSequential(d3.interpolateGreens)
-      .domain([0, 50]);
-    const textColorScale = d3.scaleSequential(d3.interpolateOranges)
-      .domain([50, 0]);
+    const colorScale = d3.scaleSequential(d3.interpolateGreens).domain([0, 10]);
+    const textColorScale = d3.scaleSequential(d3.interpolateOranges).domain([50, 0]);
 
-      //.domain([d3.min(data, row => d3.min(row)), d3.max(data, row => d3.max(row))]);
-
-    const svg = d3.select(d3Chart.current)
+    const svg = d3
+      .select(d3Chart.current)
       .attr('width', width + margin.left + margin.right)
       .attr('height', height + margin.top + margin.bottom)
       .append('g')
       .attr('transform', `translate(${margin.left},${margin.top})`);
 
-    svg.selectAll('rect')
+    svg
+      .selectAll('rect')
       .data(data.flat())
-      .enter().append('rect')
+      .enter()
+      .append('rect')
       .attr('x', (d, i) => (i % data[0].length) * gridSize)
       .attr('y', (d, i) => Math.floor(i / data[0].length) * gridSize)
       .attr('width', gridSize)
       .attr('height', gridSize)
       .attr('fill', d => colorScale(d));
-      
 
-    // Optional: Add text for each cell (showing the value)
-    svg.selectAll('text')
+    svg
+      .selectAll('text')
       .data(data.flat())
-      .enter().append('text')
+      .enter()
+      .append('text')
       .attr('x', (d, i) => (i % data[0].length) * gridSize + gridSize / 2)
       .attr('y', (d, i) => Math.floor(i / data[0].length) * gridSize + gridSize / 2)
       .attr('text-anchor', 'middle')
@@ -59,7 +60,33 @@ const Heatmap = () => {
       .text(d => d);
   };
 
-  return <svg ref={d3Chart}></svg>;
+  const className = "button", handleRandomButtonClick = () => {
+    const newData = [...data]; // Create a copy of the original data
+    const cellsToUpdate = 1;
+
+    for (let i = 0; i < cellsToUpdate; i++) {
+      const rowIndex = Math.floor(Math.random() * data.length);
+      const colIndex = Math.floor(Math.random() * data[rowIndex].length);
+
+      newData[rowIndex][colIndex] += 1;
+    }
+
+    setData(newData); // Update the state with modified data
+  };
+
+  const handleResetButtonClick = () => {
+    // Create a new array with the same dimensions as the original data, all cells set to zero
+    const resetData = data.map(row => row.map(() => 0));
+    setData(resetData); // Set the state with the reset data
+  };
+
+  return (
+    <div>
+      <button onClick={handleRandomButtonClick}>Increment Random Value</button>
+      <button onClick={handleResetButtonClick}>Reset All Cells to Zero</button>
+      <svg ref={d3Chart}></svg>
+    </div>
+  );
 };
 
 function App() {
